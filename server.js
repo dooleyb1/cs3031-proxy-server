d7e830237872e42aeb57f429a3fa5fe49505f6af
dooleyb1 <dooleyb1@tcd.ie>
2019-02-05 15:59:19 +0000

    http now working also

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 var express = require('express')
 var fs = require('fs')
 var https = require('https')
+var http = require('http')
 var app = express()
 
 app.get('/', function (req, res) {
   res.send('hello world')
 })
 
+http.createServer(app).listen(3080, function () {
+  console.log('Example app listening on port 3080! Go to http://localhost:3080/')
+})
+
 https.createServer({
   key: fs.readFileSync('certs/server.key'),
   cert: fs.readFileSync('certs/server.cert')
 }, app)
-.listen(3000, function () {
-  console.log('Example app listening on port 3000! Go to https://localhost:3000/')
+.listen(3443, function () {
+  console.log('Example app listening on port 3443! Go to https://localhost:3443/')
 })
