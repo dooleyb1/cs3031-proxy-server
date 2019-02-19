d84fff4c785a078346ada9b17173b9cc4ad1f210
dooleyb1 <dooleyb1@tcd.ie>
2019-02-19 20:16:23 +0000

    proxy somewhat better, still needs cache and blacklist

--- server.js
+++ server.js
@@ -1,26 +1,98 @@
-var fs = require('fs')
-var https = require('https')
-var http = require('http')
+var fs = require('fs');
+var https = require('https');
+var http = require('http');
 var url = require('url');
-var request = require('request')
+var request = require('request');
 
-function onRequest(req, res) {
+// Set max sockets opened by proxy
+var maxSocks = process.env.MAX_SOCKETS || 2048;
 
-    // Parse out / data
-    var queryData = url.parse(req.url, true).query;
+// Set timeout (ms) for proxy requests
+var timeout = process.env.TIMEOUT || 25000;
 
-    // If there exists ?url=
-    if (queryData.url) {
+function handleHttpRequest(url, client_response){
 
-      console.log(queryData.url);
+  http.get(url, (res) => {
+    const { statusCode } = res;
+    const contentType = res.headers['content-type'];
 
-      request(queryData.url, function(error, response, body){
-        console.log('Request:', queryData.url);
-        console.log('StatusCode:', response && response.statusCode);
-      }).pipe(res)
+    let error;
+
+    //If no 200 status received, error
+    if (statusCode !== 200) {
+      error = new Error('Request Failed.\n' +
+      `Status Code: ${statusCode}`);
     }
-    else {
-        res.end("No url found");
+
+    if (error) {
+      console.error(error.message);
+
+      client_response.write(error.message);
+      client_response.end();
+      return;
+    }
+
+    res.setEncoding('utf8');
+
+    let rawData = '';
+
+    res.on('data', (chunk) => { rawData += chunk; });
+
+    res.on('end', () => {
+      client_response.write(rawData);
+      client_response.end();
+    });
+  }).on('error', (e) => {
+    console.error(`Got error: ${e.message}`);
+  });
+}
+
+function handleHttpsRequest(url, client_response){
+
+  https.get(url, (res) => {
+    const { statusCode } = res;
+    const contentType = res.headers['content-type'];
+
+    let error;
+
+    //If no 200 status received, error
+    if (statusCode !== 200) {
+      error = new Error('Request Failed.\n' +
+      `Status Code: ${statusCode}`);
+    }
+
+    if (error) {
+      console.error(error.message);
+
+      client_response.write(error.message);
+      client_response.end();
+      return;
+    }
+
+    res.setEncoding('utf8');
+
+    let rawData = '';
+
+    res.on('data', (chunk) => { rawData += chunk; });
+
+    res.on('end', () => {
+      client_response.write(rawData);
+      client_response.end();
+    });
+  }).on('error', (e) => {
+    console.error(`Got error: ${e.message}`);
+  });
+}
+
+function onRequest(client_request, client_response) {
+
+    var url = client_request.url.substring(1);
+    console.log('Received request for: ' + url);
+
+    if(url.substring(0,7) == 'http://'){
+      handleHttpRequest(url, client_response);
+    } else if(url.substring(0,8) == 'https://'){
+      handleHttpsRequest(url, client_response);
     }
 }
 
