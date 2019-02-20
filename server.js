00992abeed33c6c6f40d8493201bd3132427160f
dooleyb1 <dooleyb1@tcd.ie>
2019-02-20 18:44:21 +0000

    cache working, needs expiry checking

--- server.js
+++ server.js
@@ -3,12 +3,10 @@ var https = require('https');
 var http = require('http');
 var url = require('url');
 var request = require('request');
+const NodeCache = require( "node-cache" );
 
-// Set max sockets opened by proxy
-var maxSocks = process.env.MAX_SOCKETS || 2048;
-
-// Set timeout (ms) for proxy requests
-var timeout = process.env.TIMEOUT || 25000;
+// Create cache, never delete files unless updated by request
+const myCache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
 
 function handleHttpRequest(url, client_response){
 
@@ -17,6 +15,7 @@ function handleHttpRequest(url, client_response){
     const contentType = res.headers['content-type'];
 
     let error;
+    console.log('Expires: ' + res.headers['expires']);
 
     //If no 200 status received, error
     if (statusCode !== 200) {
@@ -50,8 +49,10 @@ function handleHttpRequest(url, client_response){
 function handleHttpsRequest(url, client_response){
 
   https.get(url, (res) => {
+
+    // Extract status code and expires from response
     const { statusCode } = res;
-    const contentType = res.headers['content-type'];
+    const expires = res.headers['expires'];
 
     let error;
 
@@ -69,16 +70,49 @@ function handleHttpsRequest(url, client_response){
       return;
     }
 
-    res.setEncoding('utf8');
+    var cacheHit = false;
 
-    let rawData = '';
-
-    res.on('data', (chunk) => { rawData += chunk; });
-
-    res.on('end', () => {
-      client_response.write(rawData);
-      client_response.end();
+    // Check cache for web page and verify expires
+    myCache.get(url, (err, value) => {
+      if( !err ){
+        if(value == undefined){
+          console.log("URL " + url + " not found in cache. Continuing with request...");
+        }else{
+          console.log("URL found in cache, returning cached page to client...");
+          client_response.write(value);
+          client_response.end();
+          cacheHit = true;
+          return;
+        }
+      }
     });
+
+    // If url not found in cache, continue with request and cache new response
+    if(!cacheHit){
+      res.setEncoding('utf8');
+
+      let rawData = '';
+
+      res.on('data', (chunk) => { rawData += chunk; });
+
+      res.on('end', () => {
+
+        myCache.set(url, rawData, (err, success) => {
+          if(!err && success){
+            console.log("Successfully added " + url + " to cache");
+          } else{
+            console.log("Failed to add " + url + " to cache");
+          }
+        })
+
+        client_response.write(rawData);
+        client_response.end();
+      });
+    } else{
+      console.log("Cache hit, returning...");
+      return;
+    }
+
   }).on('error', (e) => {
     console.error(`Got error: ${e.message}`);
   });
@@ -93,6 +127,9 @@ function onRequest(client_request, client_response) {
       handleHttpRequest(url, client_response);
     } else if(url.substring(0,8) == 'https://'){
       handleHttpsRequest(url, client_response);
+    } else {
+      client_response.write('Invalid request, please enter a valid request such as:\n\nhttp://localhost:3080/https://www.tcd.ie');
+      client_response.end();
     }
 }
 
